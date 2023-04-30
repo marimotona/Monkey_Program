502b286c73a2b138a1a20484f336c3eee2e5f8a1
marimotona <ssmmo.06021224@gmail.com>
2023-04-30 13:15:38 +0900

    errorテスト作成

--- 02/src/monkey/parser/parser_test.go
+++ 02/src/monkey/parser/parser_test.go
@@ -8,14 +8,15 @@ import (
 
 func TestLetStatements(t *testing.T) { //具体的なテストケースの作成
 	input := `
-let x = 5;
+let x 5;
 let y = 10;
-let foobar = 838383;
+let 838383;
 `
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -38,6 +39,19 @@ let foobar = 838383;
 			return
 		}
 	}
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool { //構文解析結果を検証する
